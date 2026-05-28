e46c8dd943cc78b31e02654cbf133f1af1ad5f82
Jerry237237 <donjerrymandelsone@gmail.com>
2026-05-28 10:12:52 +0200

    Jalon 2 : Le Bus de Données (API Context)

---
+++ PolyQuiz/src/context/UserContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react";
+
+export const UserContext = createContext(null);
+
+function UserProvider({ children }) {
+  const [pseudo, setPseudo] = useState(null);
+  const [meilleurScore, setMeilleurScore] = useState(0);
+
+  return (
+    <UserContext.Provider value={{
+      pseudo,
+      setPseudo,
+      meilleurScore,
+      setMeilleurScore
+    }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export default UserProvider;
