c981a856d18cbb47f01cbddc1bfc1178560b0dd7
Jerry237237 <donjerrymandelsone@gmail.com>
2026-06-02 04:49:40 +0200

    Jalon 3 : Architecture MVC (Routes et Contrôleurs)*

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
+
+app.use(express.json());
+
+app.use('/api/tasks', taskRoutes);
+
+app.get('/api/ping', (req, res) => {
+  res.json({ message: "Serveur TaskFlow operationnel" });
+});
 
 mongoose.connect(MONGO_URI)
   .then(() => {
@@ -17,8 +26,3 @@ mongoose.connect(MONGO_URI)
   .catch((err) => {
     console.error('Erreur de connexion à MongoDB :', err.message);
   });
-
-app.get('/api/ping', (req, res) => {
-  res.json({ message: "Serveur TaskFlow operationnel" });
-});
-
