172885889c073f6ad73ce9e969b44b809c584c24
Jerry237237 <donjerrymandelsone@gmail.com>
2026-06-02 02:16:44 +0200

    Jalon 1 : Initialisation et Serveur Express

---
+++ server/server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/api/ping', (req, res) => {
+  res.json({ message: "Serveur TaskFlow operationnel" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
